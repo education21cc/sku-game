94de0b498beeb27835326c8d562bc7457fce4f21
Wouter van den Heuvel <exo_00@hotmail.com>
2021-10-14 00:28:34 +0200

    Updates

--- precache-manifest.f5483ce812075a002ce3b80f891b21cb.js
+++ precache-manifest.f5483ce812075a002ce3b80f891b21cb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4e0b5ed710a74cf5cc4aa534fc01ac7",
+    "revision": "e120d4e9c8ced2fd9b140c40cda5a076",
     "url": "./index.html"
   },
   {
-    "revision": "c9c41ed7d5892e60280a",
+    "revision": "96f7e1f82a6ff4e34728",
     "url": "./static/css/main.db410daf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.f7caa06a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c9c41ed7d5892e60280a",
-    "url": "./static/js/main.0f1aadd7.chunk.js"
+    "revision": "96f7e1f82a6ff4e34728",
+    "url": "./static/js/main.c2a09492.chunk.js"
   },
   {
     "revision": "1d750820ec3e8ff32a3f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.37002ec352663dbde2fdfd24a9a69872.js"
+  "./precache-manifest.f5483ce812075a002ce3b80f891b21cb.js"
 );
 
 self.addEventListener('message', (event) => {
